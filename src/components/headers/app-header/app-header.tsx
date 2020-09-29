9eacb29003580ca367eb1ae37868d9b1784ff614
NinZZya <s.a.azarov@gmail.com>
2020-09-29 10:20:06 +0300

    Review headers

--- src/components/headers/app-header/app-header.tsx
+++ src/components/headers/app-header/app-header.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 const AppHeader = () => {
   return (
-    <div></div>
+    <nav className="header d-flex">
+      <p className="header-logo">
+        <a href="#" >
+          Star Wars DB
+        </a>
+      </p>
+      <ul className="d-flex">
+        <li>
+          <a href="#">People</a>
+        </li>
+        <li>
+          <a href="#">Planets</a>
+        </li>
+        <li>
+          <a href="#">Starships</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
