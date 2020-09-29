de5ac37e097804392f1eb26a8d2504acaa3443c1
NinZZya <s.a.azarov@gmail.com>
2020-09-29 09:19:59 +0300

    Review index

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 import './main.scss';
-import SwapiService from './services/swapi-service';
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
+
