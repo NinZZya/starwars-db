f8e169c28f2f2d10eba38d3c24850e281dd56539
NinZZya <s.a.azarov@gmail.com>
2020-09-29 10:20:33 +0300

    Review App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,25 @@
+import ListItems from 'components/lists/list-elements';
 import React from 'react';
+import AppHeader from '../headers/app-header';
+import RandomPlanet from '../random/random-planet';
+import ListElements from '../lists/list-elements';
+import DetailsPerson from '../details/details-person';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <AppHeader />
+      <RandomPlanet />
+
+      <div className="row mb-2">
+        <div className="col-mb-6">
+          <ListElements />
+        </div>
+        <div className="col-mb-6">
+          <DetailsPerson />
+        </div>
+      </div>
+    </div>
   );
 };
 
