960bd7753c1e40abc114df746af8b572ba6c7087
NinZZya <s.a.azarov@gmail.com>
2020-09-29 16:58:47 +0300

    Add adapter. Code review

--- src/services/swapi-service.ts
+++ src/services/swapi-service.ts
@@ -1,3 +1,31 @@
+interface IRPerson {
+  url: string;
+  name: string;
+  gender: string;
+  birthYear: string;
+  eyeColor: string;
+}
+
+interface IRPlanet {
+  url: string;
+  name: string;
+  population: number;
+  rotation_period: number;
+  diameter: number;
+}
+
+interface IRStarship {
+  url: string;
+  name: string;
+  model: string;
+  manufacturer: string;
+  costInCredits: number;
+  length: number;
+  crew: number;
+  passengers: number;
+  cargoCapacity: number;
+}
+
 
 export default class SwapiService {
   _baseUrl = 'https://swapi.dev/api';
@@ -18,32 +46,73 @@ export default class SwapiService {
 
   async getAllPersons() {
     const responce = await this.getResource(this._personsUrl);
-    return responce.results;
+    return responce.results.map(this._adaptPerson);
   }
 
   async getPerson(id: string) {
     const responce = await this.getResource(`${this._personsUrl}${id}`);
-    return responce;
+    return this._adaptPerson(responce);
   }
 
   async getAllPlanets() {
     const responce = await this.getResource(this._planetsUrl);
-    return responce.results;
+    return responce.results.map(this._adaptPlanet);
   }
 
   async getPlanet(id: string) {
     const responce = await this.getResource(`${this._planetsUrl}${id}`);
-    return responce;
+    return this._adaptPlanet(responce);
   }
 
   async getAllStarships() {
     const responce = await this.getResource(this._starshipsUrl);
-    return responce.results;
+    return responce.results.map(this._adaptStarship);
   }
 
   async getStarship(id: string) {
     const responce = await this.getResource(`${this._starshipsUrl}${id}`);
-    return responce;
+    return this._adaptStarship(responce);
+  }
+
+
+  _extractId(item: IRPerson | IRPlanet | IRStarship) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    const id = item.url.match(idRegExp);
+    return id ? id[1] : '';
+  }
+
+  _adaptPerson(person: IRPerson) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor,
+    }
+  }
+
+  _adaptPlanet(planet: IRPlanet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: Number(planet.population),
+      rotationPeriod: Number(planet.rotation_period),
+      diameter: Number(planet.diameter),
+    };
+  }
+
+  _adaptStarship(starship: IRStarship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: Number(starship.costInCredits),
+      length: Number(starship.length),
+      crew: Number(starship.crew),
+      passengers: Number(starship.passengers),
+      cargoCapacity: Number(starship.cargoCapacity),
+    }
   }
 }
 
