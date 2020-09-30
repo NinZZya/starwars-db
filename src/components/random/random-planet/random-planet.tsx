b376128e653fae4338818d21df908afa0f554135
NinZZya <s.a.azarov@gmail.com>
2020-09-30 10:08:50 +0300

    WIP. Add ErrorMEssage to RandomPlanet. Code review.

--- src/components/random/random-planet/random-planet.tsx
+++ src/components/random/random-planet/random-planet.tsx
@@ -1,56 +1,75 @@
 import React, { PureComponent } from 'react';
 import Spiner from '../../spiner';
+import ErrorMessage from '../../error-message';
 import PlanetCard from './components/planet-card';
 import SwapiService from '../../../services/swapi-service';
+import { getRandomInt } from '../../utils/random';
 import { IPlanet } from '../../../types';
 
-interface S {
-  planet: IPlanet;
-  loading: boolean;
-}
+const TIMEOUT = 5000;
+class RandomPlanet extends PureComponent {
+  swapiService = new SwapiService();
 
-const swapiService = new SwapiService();
-class RandomPlanet extends PureComponent<{}, S>{
+  interval: NodeJS.Timeout | number | null = null;
 
-  constructor(props = {}) {
-    super(props);
+  state = {
+    planet: {
+      id: '',
+      name: '',
+      population: '',
+      rotationPeriod: 0,
+      diameter: 0,
+    },
+    loading: true,
+    error: false,
+  };
 
-    this.state = {
-      planet: {
-        id: '',
-        name: '',
-        population: 0,
-        rotationPeriod: 0,
-        diameter: 0,
-      },
-      loading: true,
-    };
-
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(
+      this.updatePlanet.bind(this),
+      TIMEOUT
+    );
   }
 
   onPlanetLoaded = (planet: IPlanet) => {
     this.setState({
       planet,
       loading: false,
+      error: false,
     });
   };
 
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  }
+
   updatePlanet() {
-    const id = '12';
-    swapiService
+    const id = String(getRandomInt(2, 17));
+
+    this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
+
+    const hasDate = !(loading || error);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spiner = loading ? <Spiner /> : null;
+    const planetCard = hasDate ? <PlanetCard planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
-        {loading ?
-        <Spiner /> :
-        <PlanetCard planet={planet} />}
+        {spiner}
+        {planetCard}
+        {errorMessage}
       </div>
 
     );
