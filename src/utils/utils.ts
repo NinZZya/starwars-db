2d023cba281e44d8504952b97c9bc3d61a417a60
NinZZya <s.a.azarov@gmail.com>
2020-10-09 00:52:12 +0300

    WIP. Add utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,12 @@
+import { TPayload } from '../types';
+
 export const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 };
+
+export const makeActionCreator = (type: string) => (payload: TPayload) => ({
+  type,
+  payload,
+});
