dd3bcf0bc04209732291ed38b608b390c29eb321
NinZZya <s.a.azarov@gmail.com>
2020-10-10 00:11:35 +0300

    Add layout Sort component

---
+++ src/components/sort/sort.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Sort = () => {
+  return (
+    <div className="btn-group sort" role="group" aria-label="Basic example">
+      <button type="button" className="btn btn-secondary">&#9650;</button>
+      <button type="button" className="btn btn-secondary">&#9660;</button>
+    </div>
+  );
+};
+
+
+export default Sort;
