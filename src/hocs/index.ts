b0c0b054a22ccfc980842ca92ee3ac47dcf59525
NinZZya <s.a.azarov@gmail.com>
2020-11-09 03:06:48 +0300

    WIP. Review HOCs

---
+++ src/hocs/index.ts
@@ -0,0 +1,2 @@
+export { default as withActiveFlag } from './with-active-flag/with-active-flag';
+export { default as withRandom } from './with-random/with-random';
--- src/hocs/with-random/with-random.tsx
+++ src/hocs/with-random/with-random.tsx
@@ -1,30 +1,38 @@
 import React, { PureComponent } from 'react';
-import Spiner from '../../components/spiner';
-import Message from '../../components/messages/message';
-import ErrorMessage from '../../components/messages/error-message';
-import ItemDetails from '../../components/details/item-details';
-import { DataStatus } from '../../const';
-import * as Type from '../../types';
+import { Spiner } from '../../components';
+import {
+  Message,
+  ErrorMessage,
+  ItemDetails,
+} from '../../components';
+import { DataStatus } from '../../constants/constants';
+import { Id } from '../../constants/types';
+import './with-random';
 
 
-type T = typeof ItemDetails;
-type TGetItem = Type.TGetPerson | Type.TGetPlanet | Type.TGetStarship;
+type ComponentProp = typeof ItemDetails;
 
-interface P {
-  status: DataStatus;
-  items: Type.IPerson[] | Type.IPlanet[] | Type.IStarship[];
+interface Item {
+  [key: string]: string | number | Record<string, string | number>;
+  name: string;
+  image: string;
 }
 
-interface S {
-  activeId: Type.TId,
+interface WithRandomProp {
+  status: DataStatus;
+  items: Item[];
+}
+
+interface WithRandomState {
+  activeId: Id,
 }
 
 const TIMEOUT = 5000;
 
-const withRandom = (Component: T, getItem: TGetItem, timeout: number = TIMEOUT) => {
+const withRandom = (Component: ComponentProp, getItem: (id: Id) => Item, timeout: number = TIMEOUT) => {
 
-  class WithRandom extends PureComponent<P, S> {
-    constructor(props: P) {
+  class WithRandom extends PureComponent<WithRandomProp, WithRandomState> {
+    constructor(props: WithRandomProp) {
       super(props);
 
       this.state = {
@@ -56,12 +64,14 @@ const withRandom = (Component: T, getItem: TGetItem, timeout: number = TIMEOUT) 
 
       if (status === DataStatus.SUCCESS) {
         this.setState({
-          activeId: items[Math.floor(Math.random() * items.length)].id,
+          activeId: String(
+            items[Math.floor(Math.random() * items.length)].id
+          ),
         });
       }
     }
 
-    _getCard() {
+    private getCard() {
       const { status, items } = this.props;
       const { activeId } = this.state;
       const loaded = status === DataStatus.SUCCESS;
@@ -71,8 +81,8 @@ const withRandom = (Component: T, getItem: TGetItem, timeout: number = TIMEOUT) 
       }
 
       if (items.length) {
-        const id = !activeId ? items[0].id : activeId;
-        return <Component item={getItem(id)} />;
+        const id = !activeId ? items[0].id :activeId;
+        return <Component item={getItem(String(id))} />;
       }
 
       if (this.interval) {
@@ -91,7 +101,7 @@ const withRandom = (Component: T, getItem: TGetItem, timeout: number = TIMEOUT) 
       const errorMessage = error ? <ErrorMessage /> : null;
       const spiner = loading ? <Spiner /> : null;
 
-      const card = this._getCard();
+      const card = this.getCard();
 
       return (
         <div className="random-container jumbotron rounded">
